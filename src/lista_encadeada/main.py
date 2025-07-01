7a46b7885f8f65409833977367836704323d8f5e
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-01 19:51:36 -0300

    chore(main): elimina lista TODO:
    (A) implementar uma lista encadeada simples ...ok
    (B) implementar função inserirSemPrioridade(nodo)...ok
    (C) implementar função inserirComPrioridade(nodo)...ok
    (D) implementar função inserir()...ok
    (E) implementar função imprimirListaEspera()...ok
    (F) implementar função atenderPaciente()...ok
    (G) implementar menu de utilização...ok
    (H) implementar teste do sistema...ok

--- src/lista_encadeada/main.py
+++ src/lista_encadeada/main.py
@@ -16,14 +16,5 @@ def main():
     menu()
 
 
-# TODO: (A) implementar uma lista encadeada simples
-# TODO: (B) implementar função inserirSemPrioridade(nodo)
-# TODO: (C) implementar função inserirComPrioridade(nodo)
-# TODO: (D) implementar função inserir()
-# TODO: (E) implementar função imprimirListaEspera()
-# TODO: (F) implementar função atenderPaciente()
-
-# TODO: (H) implementar teste do sistema
-
 if __name__ == "__main__":
     main()
