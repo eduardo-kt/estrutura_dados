55de284e390659376ba3d2d3bcbdeea1377a6032
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-01 17:55:44 -0300

    feat(lista): adiciona método inserirComPrioridade
    - anda pela lista a partir do head e insere o nodo após todos nodos "A", mas antes de todos os nodos "V"

--- src/lista_encadeada/componentes/lista.py
+++ src/lista_encadeada/componentes/lista.py
@@ -3,6 +3,7 @@
 
 class Cartao:
     def __init__(self, cor, nro):
+        # if cor not in ("A", "V"):
         self.cor = cor
         self.nro = nro
 
@@ -59,6 +60,31 @@ class ListaHospitalar:
 
         nodo_atual.proximo = nodo
 
+    def inserirComPrioridade(self, nodo):
+        if self.head is None:
+            self.head = nodo
+            return
+
+        if nodo.dado.cor == "A":
+            self.inserirNoInicio(nodo)
+            return
+
+        nodo_anterior = None
+        nodo_atual = self.head
+
+        while nodo_atual is not None:
+            if nodo_atual.dado.cor == "V":
+                break
+            nodo_anterior = nodo_atual
+            nodo_atual = nodo_atual.proximo
+
+        if nodo_anterior is None:
+            nodo.proximo = self.head
+            self.head = nodo
+        else:
+            nodo.proximo = nodo_atual
+            nodo_anterior.proximo = nodo
+
     def deletar(self, cartao):
         if self.head is None:
             raise Exception("Lista vazia")
@@ -93,11 +119,11 @@ if __name__ == "__main__":
     Teste = ListaHospitalar()
 
     # Inserções no início (ordem inversa na lista final)
-    Teste.inserirNoInicio(Elemento(c4))
-    Teste.inserirNoInicio(Elemento(c3))
-    Teste.inserirNoInicio(Elemento(c2))
-    Teste.inserirNoInicio(Elemento(c1))
-    Teste.inserirNoInicio(Elemento(c7))
+    Teste.inserirComPrioridade(Elemento(c4))
+    Teste.inserirComPrioridade(Elemento(c3))
+    Teste.inserirComPrioridade(Elemento(c2))
+    Teste.inserirComPrioridade(Elemento(c1))
+    Teste.inserirComPrioridade(Elemento(c7))
 
     # Inserções no final
     Teste.inserirSemPrioridade(Elemento(c5))
