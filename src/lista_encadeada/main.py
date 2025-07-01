44167a0a8b09963810f60ce82da87d7c552fbe9b
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-01 16:12:27 -0300

    refactor(main): transfere menu principal para arquivo menu.py

--- src/lista_encadeada/main.py
+++ src/lista_encadeada/main.py
@@ -9,6 +9,13 @@ Autor: Eduardo K Teixeira
 
 """
 
+from lista_encadeada.componentes.menu import menu
+
+
+def main():
+    menu()
+
+
 # TODO: (A) implementar uma lista encadeada simples
 # TODO: (B) implementar função inserirSemPrioridade(nodo)
 # TODO: (C) implementar função inserirComPrioridade(nodo)
@@ -18,49 +25,5 @@ Autor: Eduardo K Teixeira
 
 # TODO: (H) implementar teste do sistema
 
-
-# (G) implementar menu de utilização do sistema
-def menu():
-    while True:
-        print(
-            """
-               ________________________
-              | Atendimento Hospitalar |
-               ------------------------
-              """
-        )
-        print("1 - Adicionar paciente a fila")
-        print("2 - Mostrar pacientes na fila")
-        print("3 - Chamar paciente")
-        print("4 - Sair")
-
-        option = choose_option()
-
-        if option == "1":
-            print("Funcao Inserir()\n")
-            # função inserir()
-        elif option == "2":
-            print("Funcao ImprimirLista()\n")
-            # função imprimirListaEspera()
-        elif option == "3":
-            print("Funcao atenderPaciente()\n")
-            # função atenderPaciente()
-        elif option == "4":
-            print("Saindo do programa...")
-            break
-        else:
-            print("Invalid Option...")
-
-
-def choose_option():
-    try:
-        option = input("Digite sua option: ")
-    except Exception:
-        print("Digite uma option valid")
-        option = "99"
-
-    return option
-
-
 if __name__ == "__main__":
-    menu()
+    main()
