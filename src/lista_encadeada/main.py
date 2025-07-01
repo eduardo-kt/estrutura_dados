4f891a8e7dda62316d15bacfd26a875f14b85d84
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-01 15:22:16 -0300

    chore(main): adiciona lista de TODOs do projeto

---
+++ src/lista_encadeada/main.py
@@ -0,0 +1,16 @@
+# main.py
+# Projeto: Lista Encadeada
+# Autor: Eduardo Teixeira
+# Descrição: Aplicativo que gerencia lista de pacientes em espera
+
+# TODO: (A) implementar uma lista encadeada simples
+# TODO: (B) implementar função inserirSemPrioridade(nodo)
+# TODO: (C) implementar função inserirComPrioridade(nodo)
+# TODO: (D) implementar função inserir()
+# TODO: (E) implementar função imprimirListaEspera()
+# TODO: (F) implementar função atenderPaciente()
+# TODO: (G) implementar menu de utilização do sistema
+# TODO: (H) implementar teste do sistema
+
+if __name__ == "__main__":
+    print("teste arquivo main")
