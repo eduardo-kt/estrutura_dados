f0b878ecc8d60de4f54ab8fb7859c1787a07b956
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-01 20:05:20 -0300

    chore: adiciona link para github

--- src/lista_encadeada/main.py
+++ src/lista_encadeada/main.py
@@ -6,7 +6,7 @@ Lista Encadeada - fluxo principal do projeto
 Descrição: Aplicativo que gerencia lista de pacientes em espera
 
 Autor: Eduardo K Teixeira
-
+github: https://github.com/eduardo-kt/estrutura_dados.git
 """
 
 from lista_encadeada.componentes.menu import menu
