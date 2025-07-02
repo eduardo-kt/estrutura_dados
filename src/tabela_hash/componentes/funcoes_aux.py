da6537465628243c475a1174c7500f9f98411efb
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-02 18:59:11 -0300

    feat(funcoes_aux): adiciona função remover_estado
    - remover_estado: remove posição em tabela hash

--- src/tabela_hash/componentes/funcoes_aux.py
+++ src/tabela_hash/componentes/funcoes_aux.py
@@ -17,36 +17,10 @@ def inserir_estado(tabela_hash, n):
         print("Já existe um dado neste lugar!")
 
 
-# Programa principal
-
-
-"""
-while True:
-    print("1 - Inserir na tabela hash")
-    print("2 - Remover na tabela hash")
-    print("3 - Listar a tabela hash")
-    print("4 - Sair")
-
-    op = int(input("Escolha uma opção:"))
-    if op == 1:
-        chave = input("Digite a sigla de um estado: ")
-        pos = hashFuncSigla(chave, n)
-        if tabelaHash[pos] is None:
-            tabelaHash[pos] = chave
-        else:
-            print("Já existe um dado neste lugar!")
-    elif op == 2:
-        chave = int(input("Digite o que deseja remover: "))
-        pos = hashFuncSigla(chave, n)
-        if tabelaHash[pos] == chave:
-            tabelaHash[pos] = None
-        else:
-            print("Valor não localizado para a remoção!")
-    elif op == 3:
-        print(tabelaHash)
-    elif op == 4:
-        print("Encerrando...")
-        break
+def remover_estado(tabela_hash, n):
+    chave = int(input("Digite o que deseja remover: "))
+    pos = hashFuncSigla(chave, n)
+    if tabela_hash[pos] == chave:
+        tabela_hash[pos] = None
     else:
-        print("Selecione outra opção!\n")
-"""
+        print("Valor não localizado para a remoção!")
