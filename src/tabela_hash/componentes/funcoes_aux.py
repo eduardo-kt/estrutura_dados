564be2ba749f042b9345fbe3908bf036ce2b6b67
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-02 17:05:04 -0300

    feat(funcoes_aux): adiciona hashFunc, hashFuncSigla e inserir_estado
    - hashFunc: função de dispersão simples
    - hashFuncSigla: função para criar posição numérica
    - inserir_estado: função para receber entrada do usuário e posiciar esta entrada na hash table

---
+++ src/tabela_hash/componentes/funcoes_aux.py
@@ -0,0 +1,52 @@
+def hashFunc(k, n):
+    return k % n
+
+
+def hashFuncSigla(k, n):
+    k = list(k)
+    return (ord(k[0]) + ord(k[1])) % n
+
+
+def inserir_estado(tabela_hash, n):
+    chave = input("Digite a sigla de um estado: ")
+    chave = chave.strip()
+    pos = hashFuncSigla(chave, n)
+    if tabela_hash[pos] is None:
+        tabela_hash[pos] = chave
+    else:
+        print("Já existe um dado neste lugar!")
+
+
+# Programa principal
+
+
+"""
+while True:
+    print("1 - Inserir na tabela hash")
+    print("2 - Remover na tabela hash")
+    print("3 - Listar a tabela hash")
+    print("4 - Sair")
+
+    op = int(input("Escolha uma opção:"))
+    if op == 1:
+        chave = input("Digite a sigla de um estado: ")
+        pos = hashFuncSigla(chave, n)
+        if tabelaHash[pos] is None:
+            tabelaHash[pos] = chave
+        else:
+            print("Já existe um dado neste lugar!")
+    elif op == 2:
+        chave = int(input("Digite o que deseja remover: "))
+        pos = hashFuncSigla(chave, n)
+        if tabelaHash[pos] == chave:
+            tabelaHash[pos] = None
+        else:
+            print("Valor não localizado para a remoção!")
+    elif op == 3:
+        print(tabelaHash)
+    elif op == 4:
+        print("Encerrando...")
+        break
+    else:
+        print("Selecione outra opção!\n")
+"""
