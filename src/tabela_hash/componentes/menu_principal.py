c888aad08018e332c9056d2500ede6157bf2cae2
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-02 16:39:52 -0300

    feat(menu_principal): adiciona função do menu principal

---
+++ src/tabela_hash/componentes/menu_principal.py
@@ -0,0 +1,36 @@
+# src/tabela_hash/componentes/menu_principal.py
+
+
+def menu():
+
+    while True:
+        print(
+            """
+     _______________________
+    | EMPLACAMENTO VEICULAR |
+     -----------------------
+"""
+        )
+        print("1 - Inserir na tabela hash")
+        print("2 - Remover na tabela hash")
+        print("3 - Listar a tabela hash")
+        print("4 - Sair")
+
+        opcao = input("Escolha uma opcao: ")
+        opcao = opcao.strip()
+
+        if opcao == "1":
+            print("FUNÇÃO INSERIR NA TABELA HASH...")
+        elif opcao == "2":
+            print("FUNÇÃO REMOVER NA TABELA HASH")
+        elif opcao == "3":
+            print("FUNÇÃO LISTAR A TABELA HASH")
+        elif opcao == "4":
+            print("Saindo do programa...")
+            break
+        else:
+            print("Opcao invalida. Retornando ao menu principal...")
+
+
+if __name__ == "__main__":
+    menu()
