c7fb29859a4e27a63820ba097fd4699188f16c99
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-02 18:57:12 -0300

    refactor(config): modifica formato de constante INICIAL para namedtuple
    - campos tabelahash e n

--- src/tabela_hash/componentes/config.py
+++ src/tabela_hash/componentes/config.py
@@ -1,3 +1,7 @@
+from collections import namedtuple
+
 N = 10
 TABELAHASH = [None] * N
-INICIAL = [TABELAHASH, N]
+
+setup = namedtuple("setup", ["tabelahash", "n"])
+INICIAL = setup(tabelahash=TABELAHASH, n=N)
